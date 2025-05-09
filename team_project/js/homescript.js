40a0864379b79735b6be1d581ac1b47e8e57ef8d
llvovsky <lvovsky.lev@gmail.com>
2025-05-09 07:56:34 -0700

    Update homescript.js

--- team_project/js/homescript.js
+++ team_project/js/homescript.js
@@ -1,14 +1,16 @@
     const form = document.getElementById('journalForm'); // gets the form element with from the HTML
     const entriesContainer = document.getElementById('entriesContainer'); // this is a container where journal entries will appear
 
-    form.addEventListener('submit', function (e) {    // adds an event listener to the form
+    // adds an event listener to the form
+    form.addEventListener('submit', function (e) {    
       e.preventDefault();
 
       const subject = document.getElementById('subject').value; //get the user's input
       const notes = document.getElementById('notes').value;   //get the user's input
       const date = new Date().toLocaleDateString(); // representing the current date and time
 
-        // this builds HTML string
+      // this builds HTML string
+        
       const entryHTML = ` 
         <div class="entry">
           <h3>${subject}</h3>
